6342503c61eeb48955052c779655ab5a6e3e7646
marconez777 <38899187+marconez777@users.noreply.github.com>
2025-05-14 17:55:16 -0300

    Delete src/components/SubscriptionFloatingNotification.tsx

--- src/components/SubscriptionFloatingNotification.tsx
+++
@@ -1,7 +0,0 @@
-
-import React from 'react';
-
-// Empty component that does nothing as we've removed subscription functionality
-export const SubscriptionFloatingNotification = () => {
-  return null;
-};
